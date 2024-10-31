f1b94c1be7d7ceecb1de5f45e171cde723a27e18
Denes Takacs <denes.takacs@protonmail.com>
2024-10-31 21:22:49 +0100

    [backend](feat): add db context

---
+++ backend/src/PixelParadise.Infrastructure/PixelParadiseContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PixelParadise.Domain.Entities;
+
+namespace PixelParadise.Infrastructure;
+
+public class PixelParadiseContext(DbContextOptions<PixelParadiseContext> options) : DbContext(options)
+{
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Rental> Rentals => Set<Rental>();
+    public DbSet<Booking> Bookings => Set<Booking>();
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // initialise Booking 
+        modelBuilder
+            .Entity<Booking>()
+            .Property(b => b.Status)
+            .HasConversion(
+                b => b.ToString(),
+                b => Enum.Parse<BookingStatus>(b));
+        
+        base.OnModelCreating(modelBuilder);
+    }
+}
